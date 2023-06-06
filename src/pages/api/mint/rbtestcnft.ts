32857d78175ede7d0aa3d66264833a6821944533
ritchb <ritchb001@hotmail.com>
2023-06-06 16:29:51 -0400

    commit 15

--- src/pages/api/mint/rbtestcnft.ts
+++ src/pages/api/mint/rbtestcnft.ts
@@ -46,7 +46,7 @@ async function post(
 
     const user = new PublicKey(accountField);
 
-    const [transaction, message] = await createTransaction(user, tree, authority);
+    const [transaction, message] = await createTransaction(user: PublicKey);
 
     // Serialize and return the unsigned transaction.
     const serializedTransaction = transaction.serialize({
@@ -79,103 +79,102 @@ export default async function handler(
     }
 
 
-async function createTransaction(user: PublicKey, tree: PublicKey, authority: PublicKey): Promise<[Transaction, string]> {
+    async function createTransaction(user: PublicKey): Promise<[Transaction, string]> {
+
+        const authority = Keypair.fromSecretKey(
+            new Uint8Array(JSON.parse(process.env.AUTHORITY_KEY)),
+        ); // tree and collection authority
+
+        const tree = new PublicKey("ERkzt2Zyau5nnSf877FCQNzQRRxW5xaMJEt4DQhYX97T");
+
+        const collectionMint = new PublicKey("3XfkDtSZZ586DztsjeVpTV3TLMYHRci2tkwTBoGzFvfz");
+
+        const price = 0 * LAMPORTS_PER_SOL;
+
+        // Build Transaction
+        const ix = await createMintCNFTInstruction(tree, collectionMint, user, authority.publicKey);
+
+        let transaction = new Transaction();
+        transaction.add(ix);
+
+        const connection = new Connection('https://api.devnet.solana.com')
+        const bh = await connection.getLatestBlockhash();
+        transaction.recentBlockhash = bh.blockhash;
+        transaction.feePayer = authority.publicKey     // user; -> C'est moi qui paye le frais de transaction. Ca peut etre le user aussi si je veux. 
+
+        // for correct account ordering 
+        transaction = Transaction.from(transaction.serialize({
+            verifySignatures: false,
+            requireAllSignatures: false,
+        }));
+
+        transaction.sign(authority);
+        const message = 'minting rb cNFT'
+        return [transaction, message];
 
 
-    const authority = Keypair.fromSecretKey(
-        new Uint8Array(JSON.parse(process.env.AUTHORITY_KEY)),
-    ); // tree and collection authority
+        async function createMintCNFTInstruction(merkleTree: PublicKey, collectionMint: PublicKey, user: PublicKey, authority: PublicKey) {
 
-    const tree = new PublicKey("ERkzt2Zyau5nnSf877FCQNzQRRxW5xaMJEt4DQhYX97T");
+            const [treeAuthority, _bump] = PublicKey.findProgramAddressSync(
+                [merkleTree.toBuffer()],
+                BUBBLEGUM_PROGRAM_ID,
+            );
 
-    const collectionMint = new PublicKey("3XfkDtSZZ586DztsjeVpTV3TLMYHRci2tkwTBoGzFvfz");
+            const [collectionMetadataAccount, _b1] = PublicKey.findProgramAddressSync(
+                [
+                    Buffer.from("metadata", "utf8"),
+                    TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+                    collectionMint.toBuffer(),
+                ],
+                TOKEN_METADATA_PROGRAM_ID
+            );
+            const [collectionEditionAccount, _b2] = PublicKey.findProgramAddressSync(
+                [
+                    Buffer.from("metadata", "utf8"),
+                    TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+                    collectionMint.toBuffer(),
+                    Buffer.from("edition", "utf8"),
+                ],
+                TOKEN_METADATA_PROGRAM_ID
+            );
+            const [bgumSigner, __] = PublicKey.findProgramAddressSync(
+                [Buffer.from("collection_cpi", "utf8")],
+                BUBBLEGUM_PROGRAM_ID
+            );
 
-    const price = 0 * LAMPORTS_PER_SOL;
+            const ix = await createMintToCollectionV1Instruction({
+                treeAuthority: treeAuthority,
+                leafOwner: user,
+                leafDelegate: user,
+                merkleTree: merkleTree,
+                payer: user,
+                treeDelegate: authority,
+                logWrapper: SPL_NOOP_PROGRAM_ID,
+                compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+                collectionAuthority: authority,
+                collectionAuthorityRecordPda: BUBBLEGUM_PROGRAM_ID,
+                collectionMint: collectionMint,
+                collectionMetadata: collectionMetadataAccount,
+                editionAccount: collectionEditionAccount,
+                bubblegumSigner: bgumSigner,
+                tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+            }, {
+                metadataArgs: {
+                    collection: { key: collectionMint, verified: false },
+                    creators: [],
+                    isMutable: true,
+                    name: "Les castors",
+                    primarySaleHappened: true,
+                    sellerFeeBasisPoints: 0,
+                    symbol: "CAS",
+                    uri: "https://shdw-drive.genesysgo.net/BBayKe9v2acgiM6LpEio9dA1nxHHg2S6UsYrZuTVxZZL/cNFTrb_metadata.json",
+                    uses: null,
+                    tokenStandard: TokenStandard.NonFungible,
+                    editionNonce: null,
+                    tokenProgramVersion: TokenProgramVersion.Original
+                }
+            });
 
-    // Build Transaction
-    const ix = await createMintCNFTInstruction(tree, collectionMint, user, authority.publicKey);
-
-    let transaction = new Transaction();
-    transaction.add(ix);
-
-    const connection = new Connection('https://api.devnet.solana.com')
-    const bh = await connection.getLatestBlockhash();
-    transaction.recentBlockhash = bh.blockhash;
-    transaction.feePayer = authority.publicKey     // user; -> C'est moi qui paye le frais de transaction. Ca peut etre le user aussi si je veux. 
-
-    // for correct account ordering 
-    transaction = Transaction.from(transaction.serialize({
-        verifySignatures: false,
-        requireAllSignatures: false,
-    }));
-
-    transaction.sign(authority);
-    const message = 'minting cNFT'
-    return [transaction, message];
-
-
-    async function createMintCNFTInstruction(merkleTree: PublicKey, collectionMint: PublicKey, user: PublicKey, authority: PublicKey) {
-
-        const [treeAuthority, _bump] = PublicKey.findProgramAddressSync(
-            [merkleTree.toBuffer()],
-            BUBBLEGUM_PROGRAM_ID,
-        );
-
-        const [collectionMetadataAccount, _b1] = PublicKey.findProgramAddressSync(
-            [
-                Buffer.from("metadata", "utf8"),
-                TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-                collectionMint.toBuffer(),
-            ],
-            TOKEN_METADATA_PROGRAM_ID
-        );
-        const [collectionEditionAccount, _b2] = PublicKey.findProgramAddressSync(
-            [
-                Buffer.from("metadata", "utf8"),
-                TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-                collectionMint.toBuffer(),
-                Buffer.from("edition", "utf8"),
-            ],
-            TOKEN_METADATA_PROGRAM_ID
-        );
-        const [bgumSigner, __] = PublicKey.findProgramAddressSync(
-            [Buffer.from("collection_cpi", "utf8")],
-            BUBBLEGUM_PROGRAM_ID
-        );
-
-        const ix = await createMintToCollectionV1Instruction({
-            treeAuthority: treeAuthority,
-            leafOwner: user,
-            leafDelegate: user,
-            merkleTree: merkleTree,
-            payer: user,
-            treeDelegate: authority,
-            logWrapper: SPL_NOOP_PROGRAM_ID,
-            compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-            collectionAuthority: authority,
-            collectionAuthorityRecordPda: BUBBLEGUM_PROGRAM_ID,
-            collectionMint: collectionMint,
-            collectionMetadata: collectionMetadataAccount,
-            editionAccount: collectionEditionAccount,
-            bubblegumSigner: bgumSigner,
-            tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
-        }, {
-            metadataArgs: {
-                collection: { key: collectionMint, verified: false },
-                creators: [],
-                isMutable: true,
-                name: "Les castors",
-                primarySaleHappened: true,
-                sellerFeeBasisPoints: 0,
-                symbol: "CAS",
-                uri: "https://shdw-drive.genesysgo.net/BBayKe9v2acgiM6LpEio9dA1nxHHg2S6UsYrZuTVxZZL/cNFTrb_metadata.json",
-                uses: null,
-                tokenStandard: TokenStandard.NonFungible,
-                editionNonce: null,
-                tokenProgramVersion: TokenProgramVersion.Original
-            }
-        });
-
-        return ix;
+            return ix;
+        }
     }
-}
